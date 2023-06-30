c1657248366792bea7a2731a0a5b5ea6ba7b8387
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-06-29 22:35:35 -0500

    models coded

---
+++ models/thought.js
@@ -0,0 +1,71 @@
+const { Schema, model, Types } = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      require: true,
+      maxLength: 280,
+      minLength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+)
+
+function timestamp(date){
+  const newDate = date.toLocaleString();
+  return newDate;
+}
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
